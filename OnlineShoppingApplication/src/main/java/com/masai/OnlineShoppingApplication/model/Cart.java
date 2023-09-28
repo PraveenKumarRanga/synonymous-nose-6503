068b81a3a6d83d6cc6c72df4ab15f4c4c5cebecf
sushant2518 <sushantnarvekar3118@gmail.com>
2023-09-28 15:28:15 +0530

    day2

---
+++ OnlineShoppingApplication/src/main/java/com/masai/OnlineShoppingApplication/model/Cart.java
@@ -0,0 +1,35 @@
+package com.masai.OnlineShoppingApplication.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cart {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer cartId;
+	
+	@JsonIgnore
+	@OneToOne(mappedBy = "cart",cascade = CascadeType.ALL)
+	private Customer customer;
+	
+	@OneToMany(fetch = FetchType.EAGER)
+	private List<Product> products = new ArrayList<>();
+}
---
+++ OnlineShoppingApplication/src/main/java/com/masai/OnlineShoppingApplication/model/Customer.java
@@ -0,0 +1,76 @@
+package com.masai.OnlineShoppingApplication.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+	@NotBlank(message = "first name can not be blank")
+	@Size(min = 2,max = 50,message = "please provide a valid")
+	private String firstName;
+	
+	@NotBlank(message = "last name can not be blank")
+	@Size(min = 2,max = 70,message = "please provide a valid")
+	private String lastName;
+	
+	@Pattern(regexp = "^[6-9][0-9]{9}",message = "please provide valid mobile number")
+	private String mobileNumber;
+	
+	@Email(regexp = "[a-z0-9._]+@[a-z0-9._]+\\.[a-z]{2,3}",message = "please provide valid email")
+	private String email;
+	
+	@Embedded
+	private Address address;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "customer",cascade = CascadeType.ALL)
+	private List<Order> orders = new ArrayList<>();
+	
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "cart_id")
+	private Cart cart;
+
+
+	
+	public Customer(Integer userId, String password, String role,
+			@NotBlank(message = "first name can not be blank") @Size(min = 2, max = 50, message = "please provide a valid") String firstName,
+			@NotBlank(message = "last name can not be blank") @Size(min = 2, max = 70, message = "please provide a valid") String lastName,
+			@Pattern(regexp = "^[6-9][0-9]{9}", message = "please provide valid mobile number") String mobileNumber,
+			@Email(regexp = "[a-z0-9._]+@[a-z0-9._]+\\.[a-z]{2,3}", message = "please provide valid email") String email,
+			Address address) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.mobileNumber = mobileNumber;
+		this.email = email;
+		this.address = address;
+	}
+
+
+	
+	
+}
